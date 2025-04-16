09b40079bc806fdf127726276e72573478a900cb
Phoneix <1570343252@qq.com>
2025-04-16 15:56:47 +0800

    7提交-功能描述-38L

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.chi.account_book',
   appName: 'account_book_chi',
-  webDir: 'public',
+  webDir: 'out',
   server: {
     androidScheme: 'https'
   }
